f594425219764c8838db5ab7f4801d83a3d0fc6f
mojtaba_ni <mojtabanik@yahoo.com>
2025-01-18 22:04:14 +0330

    feat: add EventList component and eventHeader styles for event display

---
+++ app/widget/Events/EventList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EventHeader from "./Slice/eventHeader/eventHeader";
+
+const EventList = () => {
+  return (
+    <div>
+      <EventHeader />
+    </div>
+  );
+};
+
+export default EventList;
