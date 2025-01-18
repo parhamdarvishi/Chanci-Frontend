1a0fcc788581b5acf15afc8802f16a4711847040
mojtaba_ni <mojtabanik@yahoo.com>
2025-01-18 22:05:48 +0330

    chore: comment out unused imports and components in Events page

--- app/(pages)/Events/page.tsx
+++ app/(pages)/Events/page.tsx
@@ -1,14 +1,14 @@
-import NavbarMain from "@/shared/ui/NavbarMain/navbarMain";
-import EventList from "@/widget/Events/EventList";
-import React from "react";
+// import NavbarMain from "@/shared/ui/NavbarMain/navbarMain";
+// import EventList from "@/widget/Events/EventList";
+// import React from "react";
 
-const Events = () => {
-  return (
-    <div>
-      <NavbarMain />
-      <EventList />
-    </div>
-  );
-};
+// const Events = () => {
+//   return (
+//     <div>
+//       <NavbarMain />
+//       <EventList />
+//     </div>
+//   );
+// };
 
-export default Events;
+// export default Events;
