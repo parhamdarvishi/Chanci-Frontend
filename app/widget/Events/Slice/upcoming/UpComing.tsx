96cc1787f9499ccc4a615f6ec4ad543dcca278bf
mojtaba_ni <mojtabanik@yahoo.com>
2025-01-21 15:26:51 +0330

    feat: add UpComing component with styles and slider for upcoming events

---
+++ app/widget/Events/Slice/upcoming/UpComing.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import style from "./UpComing.module.scss";
+import { Box } from "@mantine/core";
+import Slider from "@/shared/ui/Slider/slider";
+import talk from "@public/image/slides/talk.png";
+import visa from "@public/image/slides/visa.png";
+import write from "@public/image/slides/write.png";
+import { SlideData } from "@/widget/Candidates/model";
+import Link from "next/link";
+
+const UpComing = () => {
+  const slides: SlideData[] = [
+    {
+      id: 1,
+      title: "Increasing brand awareness | Study Permit",
+      description:
+        "Increasing brand awareness and growing female engagement. Study permit services guide you through the application process.",
+      image: visa,
+    },
+    {
+      id: 2,
+      title: "Discovering hard to reach talent | Work Permit",
+      description:
+        "Discovering hard to reach talent - from awareness to successful hire, ensuring your documents are ready and deadlines met.",
+      image: write,
+    },
+    {
+      id: 3,
+      title: "Permanent Residency to reach talent | PR Card",
+      description:
+        "Study permit services guide you through the application process, ensuring your documents are ready and deadlines met.",
+      image: talk,
+    },
+    {
+      id: 4,
+      title: "Permanent Residency to reach talent | PR Card",
+      description:
+        "Study permit services guide you through the application process, ensuring your documents are ready and deadlines met.",
+      image: write,
+    },
+  ];
+  return (
+    <div className={style.wrapper}>
+      <div></div>
+      <Box className={style.detailDesc}>
+        <strong>NGN</strong>
+        hosts a variety of engaging events, including professional business
+        networking sessions, startup-focused gatherings, founder matchmaking,
+        graduate and university-focused events, job fairs, and more. Each event
+        is designed to bring together top talent and industry leaders, featuring
+        a diverse range of expert speakers who share valuable insights. Sign up
+        here to be among the first to receive invitations. We'll take care of
+        the planning so you can focus on building valuable connections!
+      </Box>
+      <Slider title={false} slides={slides} />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          paddingTop: "0rem",
+          paddingBottom: "2rem",
+        }}
+      >
+        <Link
+          href="/find-talent"
+          className={style.button}
+          style={{
+            borderRadius: "16px",
+            height: "60px",
+            fontSize: "18px",
+            fontWeight: "400",
+          }}
+        >
+          Pervious Events Gallery
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M5 12h14" />
+            <path d="m12 5 7 7-7 7" />
+          </svg>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default UpComing;
