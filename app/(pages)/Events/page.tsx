58f13adf36a7a9df892275ab63bb212395692127
mojtaba_ni <mojtabanik@yahoo.com>
2025-01-21 15:26:39 +0330

    feat: create Events page and enhance EventList component with new sections

---
+++ app/(pages)/Events/page.tsx
@@ -0,0 +1,13 @@
+import NavbarMain from "@/shared/ui/NavbarMain/navbarMain";
+import EventList from "@/widget/Events/EventList";
+
+const Events = () => {
+  return (
+    <div>
+      <NavbarMain />
+      <EventList />
+    </div>
+  );
+};
+
+export default Events;
--- app/widget/Events/EventList.tsx
+++ app/widget/Events/EventList.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import EventHeader from "./Slice/eventHeader/eventHeader";
+import UpComing from "./Slice/upcoming/UpComing";
+import { Container } from "@mantine/core";
+import Businesses from "./Slice/businesses/Businesses";
+import Comments from "./Slice/comments/Comments";
 
 const EventList = () => {
   return (
     <div>
       <EventHeader />
+      <Container>
+        <UpComing />
+        <Businesses />
+      </Container>
+      <Comments />
     </div>
   );
 };
