b89553ec947cf4e3c474a86570470a580317b63f
mojtaba_ni <mojtabanik@yahoo.com>
2025-01-24 23:49:52 +0330

    style: add upcoming events header to EventList

--- app/widget/Events/EventList.tsx
+++ app/widget/Events/EventList.tsx
@@ -5,6 +5,7 @@ import { Container } from "@mantine/core";
 import Businesses from "./Slice/businesses/Businesses";
 import Comments from "./Slice/comments/Comments";
 import EventSlider from "@/shared/ui/EventSlider/eventSlider";
+import style from "./Slice/upcoming/UpComing.module.scss";
 
 const EventList = () => {
   return (
@@ -12,6 +13,19 @@ const EventList = () => {
       <EventHeader />
 
       <Container>
+        <div className={style.upcomingHeader}>
+          <h1 style={{ fontWeight: "300" }}>
+            <strong
+              style={{ borderBottom: "4px solid #5e62fc", borderRadius: "4px" }}
+            >
+              U
+            </strong>
+            <strong>pcoming</strong>
+
+            <span> Events</span>
+          </h1>
+        </div>
+
         <EventSlider />
         <UpComing />
         <Businesses />
