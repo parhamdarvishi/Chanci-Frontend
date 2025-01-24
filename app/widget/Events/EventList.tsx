cebb9391e4c1270fed16f9a9c618bee0c0eb9eba
mojtaba_ni <mojtabanik@yahoo.com>
2025-01-24 22:25:27 +0330

    style: adjust heights in topicSection and eventHeader, and add EventSlider to EventList for improved layout and responsiveness

--- app/widget/Events/EventList.tsx
+++ app/widget/Events/EventList.tsx
@@ -4,12 +4,15 @@ import UpComing from "./Slice/upcoming/UpComing";
 import { Container } from "@mantine/core";
 import Businesses from "./Slice/businesses/Businesses";
 import Comments from "./Slice/comments/Comments";
+import EventSlider from "@/shared/ui/EventSlider/eventSlider";
 
 const EventList = () => {
   return (
     <div>
       <EventHeader />
+
       <Container>
+        <EventSlider />
         <UpComing />
         <Businesses />
       </Container>
