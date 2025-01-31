b3aecef7ff639a0627d98d08179add6a7be0009b
mojtaba_ni <mojtabanik@yahoo.com>
2025-01-31 14:07:03 +0330

    feat: implement image gallery with click-to-expand functionality

--- app/(pages)/Events/Gallery/page.tsx
+++ app/(pages)/Events/Gallery/page.tsx
@@ -1,14 +1,29 @@
 "use client";
 import { Container, Grid } from "@mantine/core";
 import Image from "next/image";
-import React from "react";
-import event1 from "@public/image/events/event1.png";
+import React, { useState } from "react";
 import event2 from "@public/image/events/event2.png";
-import event3 from "@public/image/events/event3.png";
+import Event1 from "@public/image/events/gallery/Event1.jpg";
+import Event3 from "@public/image/events/gallery/Event3.jpg";
+import Event4 from "@public/image/events/gallery/Event4.jpg";
+import Event5 from "@public/image/events/gallery/Event5.jpg";
+import Event6 from "@public/image/events/gallery/Event6.jpg";
+import Event7 from "@public/image/events/gallery/Event7.jpg";
+import Event8 from "@public/image/events/gallery/Event8.jpg";
+import Event9 from "@public/image/events/gallery/Event9.jpg";
+import Event10 from "@public/image/events/gallery/Event10.jpg";
+import Event11 from "@public/image/events/gallery/Event11.jpg";
+import Event12 from "@public/image/events/gallery/Event12.jpg";
 import style from "./gallery.module.scss";
 import NavbarMain from "@/shared/ui/NavbarMain/navbarMain";
 
-const page = () => {
+const Gallery = () => {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  const handleImageClick = (image: string) => {
+    setSelectedImage(image);
+  };
+
   return (
     <>
       <NavbarMain />
@@ -31,52 +46,143 @@ const page = () => {
         <Container className={style.galleryBox}>
           <Grid>
             <Grid.Col span={{ base: 12, md: 4 }} h={{ md: 270, sm: 240 }}>
-              <Image src={event1} alt="eventImage" className={style.img} />
+              <Image
+                src={event2}
+                alt="eventImage"
+                className={style.img}
+                loading="lazy"
+                onClick={() => handleImageClick(event2.src)}
+              />
             </Grid.Col>
             <Grid.Col span={{ base: 12, md: 4 }} h={{ md: 270, sm: 240 }}>
-              <Image src={event2} alt="eventImage" className={style.img} />
+              <Image
+                src={Event1}
+                alt="eventImage"
+                className={style.img}
+                loading="lazy"
+                onClick={() => handleImageClick(Event1.src)}
+              />
             </Grid.Col>
             <Grid.Col span={{ base: 12, md: 4 }} h={{ md: 270, sm: 240 }}>
-              <Image src={event3} alt="eventImage" className={style.img} />
+              <Image
+                src={Event3}
+                alt="eventImage"
+                className={style.img}
+                loading="lazy"
+                onClick={() => handleImageClick(Event3.src)}
+              />
             </Grid.Col>
           </Grid>
           <Grid>
             <Grid.Col span={{ base: 12, md: 3 }} h={{ md: 270, sm: 240 }}>
-              <Image src={event1} alt="eventImage" className={style.img} />
+              <Image
+                src={Event6}
+                alt="eventImage"
+                className={style.img}
+                loading="lazy"
+                onClick={() => handleImageClick(Event6.src)}
+              />
             </Grid.Col>
             <Grid.Col span={{ base: 12, md: 4 }} h={{ md: 270, sm: 240 }}>
-              <Image src={event2} alt="eventImage" className={style.img} />
+              <Image
+                src={Event5}
+                alt="eventImage"
+                className={style.img}
+                loading="lazy"
+                onClick={() => handleImageClick(Event5.src)}
+              />
             </Grid.Col>
             <Grid.Col span={{ base: 12, md: 5 }} h={{ md: 270, sm: 240 }}>
-              <Image src={event3} alt="eventImage" className={style.img} />
+              <Image
+                src={Event4}
+                alt="eventImage"
+                className={style.img}
+                loading="lazy"
+                onClick={() => handleImageClick(Event4.src)}
+              />
             </Grid.Col>
           </Grid>
           <Grid>
             <Grid.Col span={{ base: 12, md: 4 }} h={{ md: 270, sm: 240 }}>
-              <Image src={event1} alt="eventImage" className={style.img} />
+              <Image
+                src={Event7}
+                alt="eventImage"
+                className={style.img}
+                loading="lazy"
+                onClick={() => handleImageClick(Event7.src)}
+              />
             </Grid.Col>
             <Grid.Col span={{ base: 12, md: 4 }} h={{ md: 270, sm: 240 }}>
-              <Image src={event2} alt="eventImage" className={style.img} />
+              <Image
+                src={Event8}
+                alt="eventImage"
+                className={style.img}
+                loading="lazy"
+                onClick={() => handleImageClick(Event8.src)}
+              />
             </Grid.Col>
             <Grid.Col span={{ base: 12, md: 4 }} h={{ md: 270, sm: 240 }}>
-              <Image src={event3} alt="eventImage" className={style.img} />
+              <Image
+                src={Event9}
+                alt="eventImage"
+                className={style.img}
+                loading="lazy"
+                onClick={() => handleImageClick(Event9.src)}
+              />
             </Grid.Col>
           </Grid>
           <Grid>
-            <Grid.Col span={{ base: 12, md: 3 }} h={{ md: 270, sm: 240 }}>
-              <Image src={event1} alt="eventImage" className={style.img} />
+            <Grid.Col span={{ base: 12, md: 3 }} h={{ md: 280, sm: 240 }}>
+              <Image
+                src={Event10}
+                alt="eventImage"
+                className={style.img}
+                loading="lazy"
+                onClick={() => handleImageClick(Event10.src)}
+              />
             </Grid.Col>
-            <Grid.Col span={{ base: 12, md: 4 }} h={{ md: 270, sm: 240 }}>
-              <Image src={event2} alt="eventImage" className={style.img} />
+            <Grid.Col span={{ base: 12, md: 4 }} h={{ md: 280, sm: 240 }}>
+              <Image
+                src={Event11}
+                alt="eventImage"
+                className={style.img}
+                loading="lazy"
+                onClick={() => handleImageClick(Event11.src)}
+              />
             </Grid.Col>
-            <Grid.Col span={{ base: 12, md: 5 }} h={{ md: 270, sm: 240 }}>
-              <Image src={event3} alt="eventImage" className={style.img} />
+            <Grid.Col span={{ base: 12, md: 5 }} h={{ md: 280, sm: 240 }}>
+              <Image
+                src={Event12}
+                alt="eventImage"
+                className={style.img}
+                loading="lazy"
+                onClick={() => handleImageClick(Event12.src)}
+              />
             </Grid.Col>
           </Grid>
         </Container>
+
+        {selectedImage && (
+          <div className={style.selectedImageContainer}>
+            <Image
+              src={selectedImage}
+              alt="Selected Event"
+              width={500}
+              height={600}
+              quality={100}
+              className={style.selectedImage}
+            />
+            <button
+              onClick={() => setSelectedImage(null)}
+              className={style.closeButton}
+            >
+              Close
+            </button>
+          </div>
+        )}
       </Container>
     </>
   );
 };
 
-export default page;
+export default Gallery;
