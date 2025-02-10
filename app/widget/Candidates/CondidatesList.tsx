854c9a78f81b2409814516a00c912c62e7dd2a8c
mojtaba_ni <mojtabanik@yahoo.com>
2025-02-10 18:50:07 +0330

    feat: Update CandidatesList with new blog-related slides and conditional "See more" button

--- app/widget/Candidates/CondidatesList.tsx
+++ app/widget/Candidates/CondidatesList.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ClearPlan from "./Slice/ClearPlan/clearPlan";
 import Event from "./Slice/Event/event";
-import talk from "@public/image/slides/talk.png";
-import visa from "@public/image/slides/visa.png";
+import canBlogTop from "@public/image/blog/canBlogTop.png";
+import canBlogTopCourse from "@public/image/blog/canBlogTopCourse.png";
+import canBlogSmart from "@public/image/blog/canBlogSmart.png";
 import write from "@public/image/slides/write.png";
 import Slider from "@/shared/ui/Slider/slider";
 import { SlideData } from "./model";
@@ -16,31 +17,24 @@ const CondidatesList = () => {
   const slides: SlideData[] = [
     {
       id: 1,
-      title: "Increasing brand awareness | Study Permit",
+      title: "Top Growth Sectors to Watch in the UK Job Market for 2025",
       description:
-        "Increasing brand awareness and growing female engagement. Study permit services guide you through the application process.",
-      image: visa,
+        "Big changes are happening in the UK job market, shaped by tech advancements, climate goals, and shifting demographics...",
+      image: canBlogTop,
     },
     {
       id: 2,
-      title: "Discovering hard to reach talent | Work Permit",
+      title: "Top Courses Gen Z Should Check Out for a Bright Career in 2025",
       description:
-        "Discovering hard to reach talent - from awareness to successful hire, ensuring your documents are ready and deadlines met.",
-      image: write,
+        "Gen Z is stepping into a workplace full of change. To stay ahead, you need skills that match growing industries and new tech trends ...",
+      image: canBlogTopCourse,
     },
     {
       id: 3,
-      title: "Permanent Residency to reach talent | PR Card",
+      title: "7 Smart Questions to Ask Before You Hit ‘Apply",
       description:
-        "Study permit services guide you through the application process, ensuring your documents are ready and deadlines met.",
-      image: talk,
-    },
-    {
-      id: 4,
-      title: "Permanent Residency to reach talent | PR Card",
-      description:
-        "Study permit services guide you through the application process, ensuring your documents are ready and deadlines met.",
-      image: write,
+        "The job market moves fast, and it’s easy to hit ‘apply’ on every role that catches your eye. But pausing to ask yourself the right questions can save time...",
+      image: canBlogSmart,
     },
   ];
   return (
@@ -65,32 +59,34 @@ const CondidatesList = () => {
             paddingBottom: "2rem",
           }}
         >
-          <Link
-            href="/find-talent"
-            className={style.button}
-            style={{
-              borderRadius: "16px",
-              height: "60px",
-              fontSize: "20px",
-              fontWeight: "400",
-            }}
-          >
-            See more
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
+          {slides.length > 3 && (
+            <Link
+              href="/find-talent"
+              className={style.button}
+              style={{
+                borderRadius: "16px",
+                height: "60px",
+                fontSize: "20px",
+                fontWeight: "400",
+              }}
             >
-              <path d="M5 12h14" />
-              <path d="m12 5 7 7-7 7" />
-            </svg>
-          </Link>
+              See more
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M5 12h14" />
+                <path d="m12 5 7 7-7 7" />
+              </svg>
+            </Link>
+          )}
         </div>
       </div>
     </div>
