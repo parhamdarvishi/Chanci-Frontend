2afa597cc63c97d3d742366b05a5b938acc6bcbf
mojtaba_ni <mojtabanik@yahoo.com>
2025-02-14 14:21:11 +0330

    feat: Add authentication-related constants and helper exports

---
+++ app/shared/constants/relative-url/auth.ts
@@ -0,0 +1,10 @@
+export const authAddresses = {
+  loginPage: "/login",
+  login: "/Authentication/Login",
+  Logout: "/Authentication/Logout",
+  refreshToken: "/V1/Authentication/RefreshToken",
+  getCaptcha: "/Authentication/GenerateCaptcha",
+  getAuthenticatedUser: "/Authentication/GetAuthenticatedUser",
+  VerifyLoginOtp: "/Authentication/VerifyLoginOtp",
+  updatePassword: "/Authentication/UpdatePassword",
+};
