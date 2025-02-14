3d5767ce99d0e382e16a4585c6ed6752b44b0e57
mojtaba_ni <mojtabanik@yahoo.com>
2025-02-14 14:20:44 +0330

    feat: Add base API configuration with axios interceptors

---
+++ app/shared/api/chanci/base.tsx
@@ -0,0 +1,135 @@
+import axios, {
+  AxiosInstance,
+  AxiosRequestConfig,
+  CreateAxiosDefaults,
+} from "axios";
+import config from "@shared/config";
+import toastAlert from "@shared/helpers/toast";
+import { log } from "@shared/helpers";
+import cookie from "@shared/helpers/cookie";
+import { REFRESH_TOKEN, USER_TOKEN } from "@shared/helpers/cookie/types";
+import { OptionsTypes } from "@shared/api/chanci/model";
+import { authAddresses } from "@/shared/constants/relative-url/auth";
+
+const base = (
+  authorization: boolean = false,
+  excel?: boolean,
+  upload?: boolean
+): AxiosInstance => {
+  const headers = {
+    "Content-Type": upload
+      ? ("multipart/form-data" as const)
+      : ("application/json" as const),
+    Connection: "keep-alive",
+  };
+  const options: OptionsTypes = {
+    baseURL: config.ApiBaseUrl,
+    headers: headers,
+  };
+
+  if (excel) {
+    options["responseType"] = "blob";
+  }
+
+  if (authorization) {
+    options.headers["Authorization"] =
+      "Bearer " + cookie?.getCookie(USER_TOKEN);
+  }
+
+  const instance = axios.create(options as CreateAxiosDefaults<OptionsTypes>);
+
+  instance.interceptors.request.use(
+    function (config) {
+      // Do something before the request is sent
+      return config;
+    },
+    function (error) {
+      // Do something with request error
+      return Promise.reject(error);
+    }
+  );
+
+  const responseInterceptor = instance.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    function (error) {
+      if (error.response) {
+        if (error.response.status === 401) {
+          const refreshToken = cookie.getCookie(REFRESH_TOKEN);
+          if (!authorization) return;
+          if (!refreshToken) {
+            // todo: delete user from storage
+            window.location.href = "/login";
+            return;
+          }
+          options.headers["Content-Type"] = "application/json";
+          instance.interceptors.response.eject(responseInterceptor);
+          return instance
+            .post(
+              authAddresses.refreshToken,
+              { refreshToken },
+              options as AxiosRequestConfig
+            )
+            .then((response) => {
+              log("Refresh Response", response);
+              const { data } = response;
+              // todo: should change this by zustand persis
+              cookie.setCookie(
+                USER_TOKEN,
+                data.accessToken,
+                undefined,
+                data.accessTokenExpireTime
+              );
+              error.response.config.headers["Authorization"] =
+                "Bearer " + data.accessToken;
+              return axios(error.response.config);
+            })
+            .catch((error) => {
+              log("Check Refresh Error:", error.response);
+              // todo: delete user from storage by zustand
+              window.location.href = "/login";
+              return;
+            });
+        }
+        if (error.response.status >= 500 && error.response.status < 600) {
+          if (error.response.headers.expires == "-1") {
+            return;
+          }
+          toastAlert(
+            error.response.data?.message ?? error.response.statusText,
+            "error"
+          );
+        }
+
+        if (error.response.status >= 400 && error.response.status < 500)
+          toastAlert(
+            error.response.data?.Errors[0].Message ?? error.response.statusText,
+            "error"
+          );
+        if (error.response.status >= 300 && error.response.status < 400)
+          toastAlert(
+            error.response.data?.message ?? error.response.statusText,
+            "info"
+          );
+        log({ errorResponse: error.response });
+        log({ errorData: error.response.data });
+        log(error.response.status);
+        log(error.response.statusText);
+        log(error.response.headers);
+      } /*else if (error.request) {
+    // The request was made but no response was received
+    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+    // http.ClientRequest in node.js
+ 
+  }*/ else {
+        toastAlert(error.message, "error");
+        log("Error:", error.message);
+      }
+    }
+  );
+
+  return instance;
+};
+
+export default base;
