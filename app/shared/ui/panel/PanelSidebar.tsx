215c3ba1cff8f55907eec8044e2352fb2c3c5460
mojtaba_ni <mojtabanik@yahoo.com>
2025-02-20 23:25:52 +0330

    feat: Add PanelSidebar component

---
+++ app/shared/ui/panel/PanelSidebar.tsx
@@ -0,0 +1,40 @@
+import { Box, Card, Divider } from "@mantine/core";
+import React from "react";
+import style from "./style/panelSidebar.module.scss";
+import Title from "@public/image/widget/Frame.svg";
+import Image from "next/image";
+import { IconUser } from "@tabler/icons-react";
+
+const PanelSidebar = () => {
+  return (
+    <Card shadow="sm" padding="lg" className={style.wrapper}>
+      <Box>
+        <Image src={Title} alt="ChanciAi" loading="lazy" width={115} />
+      </Box>
+      <Divider color="#D5D5D7" style={{ margin: "1.6rem 0" }} />
+
+      <Box className={style.progressPart}>
+        <div className={style.progressPartBox}>
+          <IconUser />
+          <span style={{ transform: "translateY(3px)" }}>Users</span>
+        </div>
+        <div className={style.progressPartBox}>
+          <span style={{ transform: "translateY(3px)" }}>
+            Career Preference (6 Questions){" "}
+          </span>
+        </div>
+        <div className={style.progressPartBox}>
+          <span>Nationality & Visa (3 Questions)</span>
+        </div>
+        <div className={style.progressPartBox}>
+          <span>CV Section ( 1 Question)</span>
+        </div>
+        <div className={style.progressPartBox}>
+          <span>Skills & Certificate ( 2 Questions)</span>
+        </div>
+      </Box>
+    </Card>
+  );
+};
+
+export default PanelSidebar;
