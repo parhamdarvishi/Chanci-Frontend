a64a43546eb456531708221f8dd5b729790f77ee
mojtaba_ni <mojtabanik@yahoo.com>
2025-02-21 23:34:44 +0330

    feat: add chanciApi to chanci layout

--- app/(chanci)/layout.tsx
+++ app/(chanci)/layout.tsx
@@ -6,6 +6,10 @@ import { Poppins } from "next/font/google";
 import ChanciHeader from "@/shared/ui/ChanciAI/ChanciHeader";
 import Sidebar from "@/shared/ui/ChanciAI/Sidebar";
 import { ToastContainer } from "react-toastify";
+import { getRequest } from "@/shared/api";
+import { chanciAddresses } from "@/shared/constants/relative-url/chanci";
+import { useEffect } from "react";
+import { useChanci } from "@/shared/stateManagement/UseChanci/useChanci";
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700"],
@@ -18,7 +22,33 @@ export default function ChanciRootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  // const getChan
+  const { updateData } = useChanci();
+  interface ChanciResponse {
+    isSuccess?: boolean;
+    data?: {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      items?: any[]; // Adjust the type as necessary
+    };
+  }
+  const getChanciData = async () => {
+    const reqBody = {
+      Skip: 0,
+      Take: 1000,
+    };
+    const res: ChanciResponse = await getRequest(
+      chanciAddresses.GetAll,
+      reqBody,
+      true
+    );
+    if (res?.isSuccess) {
+      updateData(res?.data?.items);
+    }
+  };
+
+  useEffect(() => {
+    getChanciData();
+  }, []);
+
   return (
     <html lang="en">
       <body className={poppins.className}>
