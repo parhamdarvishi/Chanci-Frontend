9625803a9443a525eb7340f5ed613cec33e1fa62
mojtaba_ni <mojtabanik@yahoo.com>
2025-02-21 23:38:38 +0330

    feat: create TextQuestion  chanci component

---
+++ app/features/textQuestion/TextQuestion.tsx
@@ -0,0 +1,73 @@
+"use client";
+import React, { useState } from "react";
+import style from "./style.module.scss";
+import { Avatar, Box, Card, Textarea } from "@mantine/core";
+import Image from "next/image";
+import chanciIc from "@public/image/chanciAI/icon/chanciCh.svg";
+import { useChanci } from "@/shared/stateManagement/UseChanci/useChanci";
+
+interface TextQuestionProps {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  question: any;
+}
+
+const TextQuestion: React.FC<TextQuestionProps> = ({ question }) => {
+  const [value, setValue] = useState("");
+
+  const { updateQuestionIndex, updateAnswers, questionIndex } = useChanci();
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === "Enter") {
+      console.log(value);
+      const answer = {
+        questionId: question?.id,
+        answerId: 0,
+        text: value,
+      };
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      updateAnswers(answer as any);
+      updateQuestionIndex(questionIndex + 1);
+      // You can add additional logic here
+      event.preventDefault();
+    }
+  };
+  return (
+    <div className={style.wrapper}>
+      <Box className={style.userBox}>
+        <Avatar radius="xl" size={"lg"} />
+        <Box className={style.questionTitle}>
+          <p>{question?.text}</p>
+        </Box>
+      </Box>
+      <Box className={style.questionPart}>
+        <Box className={style.questionBox}>
+          <Card
+            shadow="sm"
+            padding="lg"
+            radius="md"
+            withBorder
+            className={style.questionCard}
+          >
+            <Textarea
+              classNames={{ input: style.input }}
+              placeholder="Write a messge here ..."
+              size="sm"
+              radius="md"
+              minRows={4}
+              value={value}
+              onChange={(event) => setValue(event.currentTarget.value)}
+              onKeyDown={handleKeyDown}
+            />
+          </Card>
+          <Image
+            src={chanciIc}
+            alt="chanciIcon"
+            className={style.questionImg}
+          />
+        </Box>
+      </Box>
+    </div>
+  );
+};
+
+export default TextQuestion;
