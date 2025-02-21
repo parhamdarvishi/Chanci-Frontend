f33fbfd45a724e2be871493f426fec8bf5a795c0
mojtaba_ni <mojtabanik@yahoo.com>
2025-02-21 23:40:26 +0330

    feat: add api to psychlogyTest page

--- app/widget/chanciAI/slice/PsychologyTest/psychologyTest.tsx
+++ app/widget/chanciAI/slice/PsychologyTest/psychologyTest.tsx
@@ -1,7 +1,63 @@
-import React from "react";
+import DropDownQuestion from "@/features/dropDownQuestion/DropDownQuestion";
+import OptionQuestion from "@/features/optionsQuestions/OptionQuestion";
+import ProgressQuestion from "@/features/progressQuestion/ProgressQuestion";
+import TextQuestion from "@/features/textQuestion/TextQuestion";
+import UploadQuestion from "@/features/uploadQuestion/UploadQuestion";
+import { useChanci } from "@/shared/stateManagement/UseChanci/useChanci";
+import React, { useEffect } from "react";
 
-const psychologyTest = () => {
-  return <div>psychologyTest</div>;
+// Define the type for the question prop
+interface Question {
+  inputType?: number;
+  // Add other properties as needed
+}
+
+interface PsychologyTestProps {
+  question: Question;
+}
+
+const PsychologyTest: React.FC<PsychologyTestProps> = ({ question }) => {
+  const { questionIndex, updateQuestionIndex, answers, UpdateSidebarPostion } =
+    useChanci();
+
+  console.log({ questionIndex });
+  console.log({ question });
+  console.log({ answers });
+
+  useEffect(() => {
+    if (question?.inputType === 0) {
+      updateQuestionIndex(questionIndex + 1);
+    }
+    // @ts-expect-error: Ignoring TypeScript error due to array spread
+    if (question?.type === 2) {
+      UpdateSidebarPostion(2);
+    }
+    // @ts-expect-error: Ignoring TypeScript error due to array spread
+    if (question?.type === 3) {
+      UpdateSidebarPostion(3);
+    }
+    // @ts-expect-error: Ignoring TypeScript error due to array spread
+    if (question?.type === 4) {
+      UpdateSidebarPostion(4);
+    }
+    // @ts-expect-error: Ignoring TypeScript error due to array spread
+    if (question?.type === 5) {
+      UpdateSidebarPostion(5);
+    }
+  }, [question]);
+
+  return (
+    <div style={{ height: "100%" }}>
+      {question?.inputType === 1 && (
+        <TextQuestion question={question} />
+        // <div>Text Question Component</div>
+      )}
+      {question?.inputType === 2 && <ProgressQuestion question={question} />}
+      {question?.inputType === 3 && <DropDownQuestion question={question} />}
+      {question?.inputType === 4 && <UploadQuestion />}
+      {question?.inputType === 5 && <OptionQuestion question={question} />}
+    </div>
+  );
 };
 
-export default psychologyTest;
+export default PsychologyTest;
