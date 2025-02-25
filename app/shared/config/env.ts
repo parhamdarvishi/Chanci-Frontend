b45042374f6b5a0e7f3ceacf328bd8b064050931
mojtaba_ni <mojtabanik@yahoo.com>
2025-02-25 12:55:27 +0330

    fix : change API_BASE_URL

--- app/shared/config/env.ts
+++ app/shared/config/env.ts
@@ -2,7 +2,8 @@ import process from "process";
 
 /** API Base URL */
 // export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-export const API_BASE_URL =
-  "http://ngnapi-dev.eba-gzkrmdrt.eu-west-2.elasticbeanstalk.com";
+// export const API_BASE_URL =
+//   "http://ngnapi-dev.eba-gzkrmdrt.eu-west-2.elasticbeanstalk.com";
+export const API_BASE_URL = "https://nice-ride.185-55-224-196.plesk.page";
 /** Is Development */
 export const IS_DEVELOPMENT = process.env.NODE_ENV === "development";
