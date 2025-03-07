e2c60c2645065fcb3b6e09366c6bd9000f2fd824
mojtaba_ni <mojtabanik@yahoo.com>
2025-03-07 11:32:19 +0330

    chore: update API base URL to new endpoint

--- app/shared/config/env.ts
+++ app/shared/config/env.ts
@@ -4,6 +4,9 @@ import process from "process";
 // export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 // export const API_BASE_URL =
 //   "http://ngnapi-dev.eba-gzkrmdrt.eu-west-2.elasticbeanstalk.com";
-export const API_BASE_URL = "https://nice-ride.185-55-224-196.plesk.page";
+// export const API_BASE_URL = "https://nice-ride.185-55-224-196.plesk.page";
+// export const API_BASE_URL = "https://nice-ride.185-55-224-196.plesk.page";
+// export const API_BASE_URL = "https://api10.ukngn.com/swagger/index.html";
+export const API_BASE_URL = "https://api10.ukngn.com";
 /** Is Development */
 export const IS_DEVELOPMENT = process.env.NODE_ENV === "development";
