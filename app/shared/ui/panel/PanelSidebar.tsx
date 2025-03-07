302e5b0c2ced50a423b8659fd898aa7b21d62ce1
mojtaba_ni <mojtabanik@yahoo.com>
2025-03-07 11:33:04 +0330

    feat: add navigation link to Users section in PanelSidebar

--- app/shared/ui/panel/PanelSidebar.tsx
+++ app/shared/ui/panel/PanelSidebar.tsx
@@ -4,6 +4,7 @@ import style from "./style/panelSidebar.module.scss";
 import Title from "@public/image/widget/Frame.svg";
 import Image from "next/image";
 import { IconUser } from "@tabler/icons-react";
+import Link from "next/link";
 
 const PanelSidebar = () => {
   return (
@@ -14,10 +15,10 @@ const PanelSidebar = () => {
       <Divider color="#D5D5D7" style={{ margin: "1.6rem 0" }} />
 
       <Box className={style.progressPart}>
-        <div className={style.progressPartBox}>
+        <Link href={"/panel/users"} className={style.progressPartBox}>
           <IconUser />
           <span style={{ transform: "translateY(3px)" }}>Users</span>
-        </div>
+        </Link>
         <div className={style.progressPartBox}>
           <span style={{ transform: "translateY(3px)" }}>
             Career Preference (6 Questions){" "}
