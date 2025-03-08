709f44f7cf8fe7e77841654e2ddac7c177d019af
Farid vm <flive73@gmail.com>
2025-03-08 23:09:33 +0330

    ESlinit change of components

--- app/(pages)/PaymentVerify/[token]/page.tsx
+++ app/(pages)/PaymentVerify/[token]/page.tsx
@@ -1,59 +1,12 @@
-"use client";
-import NavbarMain from "@/shared/ui/NavbarMain/navbarMain"
-import { Card } from "@mantine/core"
-import style from "./style.module.scss";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { API_BASE_URL } from "@/shared/config/env";
-import toastAlert from "@/shared/helpers/toast";
-import Header from "@/shared/ui/Header/header";
-import Footer from "@/shared/ui/Footer/footer";
-import { useParams } from "next/navigation";
-const PaymentVerify: React.FC = () => {
-    const params = useParams();
-    const { token } = params;
-    const [msg, setMsg] = useState<string>();
-    const getVerificationStatus = async () => {
-        try {
-            const res = await axios.post(
-                `${API_BASE_URL}/api/payment/verify`,
-                { code: token },
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                }
-            );
-            if (res?.data?.isSuccess && res?.data?.statusCode === 200) {
-                toastAlert("Payment Successful", "success");
-            }
-            else {
-                toastAlert("Payment was Unsuccessful", "error");
-            }
-            setMsg(res?.data?.message);
-        } catch (_error) {
-            console.log(_error);
-            toastAlert("Network Error", "error");
-            setMsg("Payment was not successful.");
-        }
-    }
-    useEffect(() => {
-        getVerificationStatus();
-    }, []);
-    return (
-        <div>
-            <Header />
-            <NavbarMain />
-            <div className={style.paymentWrapper}>
-                <Card shadow="sm" padding="lg" radius="md" withBorder className={style.paymentCard}>
-                    <p>{msg}</p>
-                </Card>
-            </div>
-            <Footer />
-        </div>
-    );
-};
+import PaymentVerify from "@/widget/Payment/PaymentVerify";
+import { Suspense } from "react";
 
+const PaymentVerifyPage = ()=> {
+    return(
+        <Suspense fallback={<div></div>}>
+        <PaymentVerify />
+      </Suspense>
+    )
+}
 
-
-export default PaymentVerify;
+export default PaymentVerifyPage;
---
+++ app/widget/Payment/Payment.tsx
@@ -0,0 +1,153 @@
+"use client";
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+/* eslint-disable no-unused-expressions */
+import React, { useState, FormEvent, ChangeEvent } from "react";
+import { Box, Card, Input, Button, Text } from "@mantine/core";
+import style from "./payment.module.scss";
+import NavbarMain from "@/shared/ui/NavbarMain/navbarMain";
+import Image from "next/image";
+import arrowRight from "@public/arrowRight.svg";
+import axios from "axios";
+import * as yup from "yup";
+import { useSearchParams } from "next/navigation";
+import { API_BASE_URL } from "@/shared/config/env";
+import toastAlert from "@/shared/helpers/toast";
+
+// Define the form data type
+interface FormData {
+  fullName: string;
+  email: string;
+}
+
+// Create a Yup schema for validation
+const schema = yup.object({
+  fullName: yup.string().min(3, "Full name must be at least 3 characters").required("Full name is required"),
+  email: yup.string().email("Invalid email format").required("Email is required"),
+});
+
+const Payment: React.FC = () => {
+  const searchParams = useSearchParams()
+  const eventId = searchParams.get('eventid')
+  const [formData, setFormData] = useState<FormData>({ fullName: "", email: "" });
+  const [errors, setErrors] = useState<{ fullName?: string; email?: string }>({});
+  const [loading, setLoading] = useState<boolean>(false);
+
+  // Update form data state on input change
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  // Validate using Yup
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setErrors({});
+
+    try {
+      // Validate the form data; abortEarly: false collects all errors
+      await schema.validate(formData, { abortEarly: false });
+    } catch (err: any) {
+      if (err.inner) {
+        const validationErrors: { [key: string]: string } = {};
+        err.inner.forEach((error: any) => {
+          if (error.path) validationErrors[error.path] = error.message;
+        });
+        setErrors(validationErrors);
+      }
+      return;
+    }
+
+    // Submit form data via Axios if validation passes
+    setLoading(true);
+    try {
+      const res = await axios.post(
+        `${API_BASE_URL}/api/event/pay`,
+        {...formData, id: Number(eventId)},
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (res?.data?.isSuccess && res?.data?.statusCode === 200) {
+        const finalRes = res?.data?.data?.data;
+        const paymenturl = finalRes.checkout_Url;
+        window.location.href = paymenturl;
+      } else {
+        toastAlert(res?.data?.message as string, "error");
+        return;
+      }
+    } catch (_error) {
+      console.log(_error);
+      toastAlert("Network Error", "error");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <NavbarMain />
+      <div className={style.paymentWrapper}>
+        <Card shadow="sm" padding="lg" radius="md" withBorder className={style.paymentCard}>
+          <p>
+            Please enter your full name and email address and click the button to proceed
+            with your payment and complete the booking process for participating in the event.
+          </p>
+          <form onSubmit={handleSubmit}>
+            <Box>
+              <Input.Wrapper
+                classNames={{
+                  root: style.root,
+                  label: style.label,
+                  description: style.description,
+                }}
+                withAsterisk
+                label="Full name"
+                description="Please enter your full name"
+              >
+                <Input
+                  name="fullName"
+                  classNames={{ input: style.input }}
+                  value={formData.fullName}
+                  onChange={handleChange}
+                />
+                {errors.fullName && <Text className={style.description} color="red" size="xs">{errors.fullName}</Text>}
+              </Input.Wrapper>
+            </Box>
+
+            <Box mt="md">
+              <Input.Wrapper
+                classNames={{
+                  root: style.root,
+                  label: style.label,
+                  description: style.description,
+                }}
+                withAsterisk
+                label="Email"
+                description="Please enter your email"
+              >
+                <Input
+                  name="email"
+                  classNames={{ input: style.input }}
+                  value={formData.email}
+                  onChange={handleChange}
+                  placeholder="For e.g. example@gmail.com"
+                />
+                {errors.email && <Text className={style.description} color="red" size="sm">{errors.email}</Text>}
+              </Input.Wrapper>
+            </Box>
+
+            <Button type="submit" className={style.button} mt="lg" loading={loading} disabled={!eventId}>
+              Pay Now
+              {!loading && (
+                <Image className={style.cardArrow} src={arrowRight} alt="arrowRight" width={22} />
+              )}
+            </Button>
+          </form>
+        </Card>
+      </div>
+    </>
+  );
+};
+
+export default Payment;
