04d7e46dbbb7bca39ff63f1706636f5269d2a9fe
mojtaba_ni <mojtabanik@yahoo.com>
2025-03-11 01:21:09 +0330

    feat: enhance MultiSelect component with dynamic interactions and state management

--- app/features/multiSelect/MultiSelect.tsx
+++ app/features/multiSelect/MultiSelect.tsx
@@ -1,18 +1,36 @@
-import { Avatar, Box, Card, MultiSelect } from "@mantine/core";
+import { Avatar, Box, Button, Card, MultiSelect } from "@mantine/core";
 import chanciIc from "@public/image/chanciAI/icon/chanciCh.svg";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import style from "./style.module.scss";
+import { IconArrowNarrowLeft, IconArrowNarrowRight } from "@tabler/icons-react";
+import { useChanci } from "@/shared/stateManagement/UseChanci/useChanci";
 
 interface DropDownQuestionProps {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   question: any;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  answers: any;
 }
 
 const MultiSelectBox = ({ question }: DropDownQuestionProps) => {
   const [dropD, setDropD] = useState([]);
+  const [hasChanges, setHasChanges] = useState(false);
+
+  const {
+    answers,
+    updateQuestionIndex,
+    updateAnswers,
+    questionIndex,
+    multiAnswer,
+    updateMultiAnswers,
+  } = useChanci();
+  const [value, setValue] = useState<string[]>(multiAnswer);
+  console.log({ answers });
+  const handleQustionIndex = () => {
+    updateQuestionIndex(questionIndex - 1);
+  };
+  const handleQustionNext = () => {
+    updateQuestionIndex(questionIndex + 3);
+  };
   const fun = () => {
     if (question?.answers?.length > 0) {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -28,10 +46,101 @@ const MultiSelectBox = ({ question }: DropDownQuestionProps) => {
       setDropD(drop);
     }
   };
+  const handleDropDown = (
+    value: string[] | ((prevState: string[]) => string[])
+  ) => {
+    setValue(value);
+    if (value?.length === 3) {
+      setHasChanges(true);
+    } else {
+      setHasChanges(false);
+    }
+    // debugger;
+    // if (answers[questionIndex]) {
+    //   const answerIndex = answers.findIndex(
+    //     (item) => item?.answerId === answers[questionIndex]?.answerId
+    //   );
+
+    //   const filterAnswer = answers; // Create a copy of the array
+    //   if (answerIndex !== -1) {
+    //     // Remove the item at answerIndex and insert the new answer
+    //     filterAnswer.splice(answerIndex, 1, {
+    //       questionId: question?.id,
+    //       answerId: Number(value),
+    //       step: Number(value),
+    //     });
+    //   }
+
+    //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    //   // updateAnswers(filterAnswer as any);
+    //   // updateQuestionIndex(questionIndex + 1);
+    //   return;
+    // }
+    // const answer = {
+    //   questionId: question?.id,
+    //   answerId: Number(value),
+    //   step: Number(value),
+    // };
+    // // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  };
+
+  const handleMulti = () => {
+    if (answers[questionIndex]?.step) {
+      const filteredAnswers = answers.filter(
+        (item) => item.questionId !== question?.id
+      );
+      // Create new answers array with the selected values
+      const newAnswers = value.map((element) => ({
+        questionId: question?.id,
+        answerId: Number(element),
+        step: Number(element),
+      }));
+
+      // Combine filtered old answers with new answers
+      const allAnswer = [...filteredAnswers, ...newAnswers];
+      updateAnswers(allAnswer);
+      updateMultiAnswers(value);
+      setValue([]);
+      updateAnswers(allAnswer);
+      updateQuestionIndex(questionIndex + 3);
+      return;
+    }
+    const allAnswer = answers;
+    const val: string[] = [];
+    value.forEach((element) => {
+      val.push(element);
+      const t = {
+        questionId: question?.id,
+        answerId: Number(element),
+        step: Number(element),
+      };
+      allAnswer.push(t);
+    });
+    updateMultiAnswers(val);
+    setValue([]);
+    updateAnswers(allAnswer);
+    updateQuestionIndex(questionIndex + 3);
+  };
+
+  const checkSubBtn = () => {
+    if (value?.length !== multiAnswer?.length) {
+      setHasChanges(true);
+      return;
+    }
+
+    const hasDifferentValues =
+      value?.some((item) => !multiAnswer?.includes(item)) ||
+      multiAnswer?.some((item) => !value?.includes(item));
+
+    setHasChanges(hasDifferentValues);
+  };
+
   useEffect(() => {
     fun();
+    checkSubBtn();
     // setVal("");
   }, [question]);
+
   return (
     <div className={style.wrapper}>
       <Box className={style.userBox}>
@@ -55,9 +164,15 @@ const MultiSelectBox = ({ question }: DropDownQuestionProps) => {
             className={style.questionCard}
           >
             <MultiSelect
-              label="Your favorite libraries"
-              placeholder="Select up to 3 libraries"
+              searchable
+              nothingFoundMessage="Nothing found..."
+              checkIconPosition="right"
+              style={{ width: "100%" }}
+              classNames={{ input: style.inpMultiSelect }}
+              label="Select up to 3 Industries"
               data={dropD}
+              value={value}
+              onChange={(value) => handleDropDown(value ? value : [])}
               maxValues={3}
               hidePickedOptions
             />
@@ -68,6 +183,81 @@ const MultiSelectBox = ({ question }: DropDownQuestionProps) => {
             className={style.questionImg}
           />
         </Box>
+        {hasChanges && (
+          <Box style={{ display: "flex", justifyContent: "end" }}>
+            <div className={style.btnChanci} onClick={handleMulti}>
+              submit
+            </div>
+          </Box>
+        )}
+      </Box>
+      <Box style={{ display: "flex", width: "100%" }}>
+        <Box
+          style={{
+            padding: "0 3rem",
+            opacity: questionIndex > 0 ? 1 : 0,
+            transform:
+              questionIndex > 0 ? "translateX(0)" : "translateX(-20px)",
+            transition: "all 0.4s cubic-bezier(0.4, 0, 0.2, 1)",
+            visibility: questionIndex > 0 ? "visible" : "hidden",
+          }}
+        >
+          <Button
+            variant="light"
+            style={{
+              padding: "0.7rem",
+              transition: "transform 0.2s ease",
+              transform: "scale(1)",
+              ":hover": {
+                transform: "scale(1.05)",
+              },
+            }}
+            onClick={handleQustionIndex}
+          >
+            <IconArrowNarrowLeft size={30} />
+          </Button>
+        </Box>
+
+        <Box
+          style={{
+            opacity: answers[questionIndex]?.step ? 1 : 0,
+            transform: answers[questionIndex]?.step
+              ? "translateX(0)"
+              : "translateX(20px)",
+            transition: "all 0.4s cubic-bezier(0.4, 0, 0.2, 1)",
+            visibility: answers[questionIndex]?.step ? "visible" : "hidden",
+          }}
+        >
+          <Button
+            variant="light"
+            style={{
+              padding: "0.7rem",
+              transition: "transform 0.2s ease",
+              transform: "scale(1)",
+              ":hover": {
+                transform: "scale(1.05)",
+              },
+            }}
+            onClick={handleQustionNext}
+          >
+            <IconArrowNarrowRight size={30} />
+          </Button>
+        </Box>
+        {answers[questionIndex]?.step && MultiSelect?.length === 3 && (
+          <Box
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: "1rem",
+              marginLeft: "400px",
+            }}
+          >
+            <div style={{ color: "#0063F5" }}>your Answer : </div>
+            {multiAnswer?.map((item: string, index: number) => (
+              <div key={index}>{item}</div>
+            ))}
+          </Box>
+        )}
       </Box>
     </div>
   );
