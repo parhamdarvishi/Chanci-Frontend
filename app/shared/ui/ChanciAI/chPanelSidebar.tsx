5928ffa532888805051bc07f839e86c0b6432b56
mojtaba_ni <mojtabanik@yahoo.com>
2025-03-18 19:11:51 +0330

    feat: Enhance ChPanelSidebar with active state management

--- app/shared/ui/ChanciAI/chPanelSidebar.tsx
+++ app/shared/ui/ChanciAI/chPanelSidebar.tsx
@@ -1,28 +1,55 @@
 import { Box, Card, Divider } from "@mantine/core";
-import React from "react";
+import React, { useState } from "react";
 import style from "./style/chPanelSidebar.module.scss";
 import Title from "@public/image/widget/Frame.svg";
 import Image from "next/image";
-import { IconReportAnalytics, IconUser } from "@tabler/icons-react";
+import {
+  IconChecklist,
+  IconReportAnalytics,
+  IconUser,
+} from "@tabler/icons-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const ChPanelSidebar = () => {
+  const [active, setActive] = useState<number>(0);
+  const { push } = useRouter();
+
+  const handleProfile = () => {
+    if (active === 0) {
+      return;
+    } else {
+      setActive(0);
+      push("/panel");
+    }
+  };
+
   return (
     <Card shadow="sm" padding="lg" className={style.wrapper}>
       <Box>
         <Image src={Title} alt="ChanciAi" loading="lazy" width={115} />
       </Box>
       <Divider color="#D5D5D7" style={{ margin: "1.6rem 0" }} />
-
       <Box className={style.progressPart}>
-        <Link href={"/panel/users"} className={style.progressPartBox}>
+        <div
+          className={
+            active === 0 ? style.progressPartBoxActive : style.progressPartBox
+          }
+          onClick={handleProfile}
+        >
           <IconUser />
           <span style={{ transform: "translateY(3px)", fontSize: "19px" }}>
             Profile
           </span>
-        </Link>
-        <Link href={"/panel/users"} className={style.progressPartBox}>
+        </div>
+        <Link href={"/ChanciAI"} className={style.progressPartBox}>
           <IconReportAnalytics />
+          <span style={{ transform: "translateY(3px)", fontSize: "19px" }}>
+            Chanci AI
+          </span>
+        </Link>
+        <Link href={"#"} className={style.progressPartBox}>
+          <IconChecklist />
           <span style={{ transform: "translateY(3px)", fontSize: "19px" }}>
             Result
           </span>
