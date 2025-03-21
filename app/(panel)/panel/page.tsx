35f26ec52d93fefb1c0d68428f2a1461db86c951
Farid vm <flive73@gmail.com>
2025-03-21 17:42:18 +0330

    dynamic panel & logout logic added

--- app/(panel)/panel/page.tsx
+++ app/(panel)/panel/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div style={{ height: "100%" }}>s</div>;
+  return <div style={{ height: "100%" }}></div>;
 };
 
 export default page;
--- app/shared/ui/panel/PanelSidebar.tsx
+++ app/shared/ui/panel/PanelSidebar.tsx
@@ -3,10 +3,27 @@ import React from "react";
 import style from "./style/panelSidebar.module.scss";
 import Title from "@public/image/widget/Frame.svg";
 import Image from "next/image";
-import { IconUser } from "@tabler/icons-react";
+import { IconLogout } from "@tabler/icons-react";
 import Link from "next/link";
+import cookie from "@/shared/helpers/cookie";
+import { useRouter } from "next/navigation";
+const LogoutButton = () => {
+  const router = useRouter();
+  const handleSubmit = () => {
+    cookie.deleteCookie("USER_TOKEN");
+    localStorage.removeItem("userMenu");
+    router.push('/');
+  }
+  return (
+    <div onClick={() => handleSubmit()} className={style.progressPartBox}>
+      <IconLogout />
+      <span style={{ transform: "translateY(3px)" }}>Logout</span>
+    </div>
+  )
+}
 
 const PanelSidebar = () => {
+  const userMenu: Array<UserMenu> = JSON.parse(localStorage.getItem("userMenu") || "[]");
   return (
     <Card shadow="sm" padding="lg" className={style.wrapper}>
       <Box>
@@ -15,24 +32,16 @@ const PanelSidebar = () => {
       <Divider color="#D5D5D7" style={{ margin: "1.6rem 0" }} />
 
       <Box className={style.progressPart}>
-        <Link href={"/panel/users"} className={style.progressPartBox}>
-          <IconUser />
-          <span style={{ transform: "translateY(3px)" }}>Users</span>
-        </Link>
-        <div className={style.progressPartBox}>
-          <span style={{ transform: "translateY(3px)" }}>
-            Career Preference (6 Questions){" "}
-          </span>
-        </div>
-        <div className={style.progressPartBox}>
-          <span>Nationality & Visa (3 Questions)</span>
-        </div>
-        <div className={style.progressPartBox}>
-          <span>CV Section ( 1 Question)</span>
-        </div>
-        <div className={style.progressPartBox}>
-          <span>Skills & Certificate ( 2 Questions)</span>
-        </div>
+        {userMenu?.map((menu, index) => {
+          return (
+
+            <Link key={`menu-${index}`} href={`${menu.link}`} className={style.progressPartBox}>
+              {/* <IconUser /> */}
+              <span style={{ transform: "translateY(3px)" }}>{menu.title}</span>
+            </Link>
+          )
+        })}
+        <LogoutButton />
       </Box>
     </Card>
   );
