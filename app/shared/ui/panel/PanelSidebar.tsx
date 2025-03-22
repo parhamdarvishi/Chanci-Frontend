4e393dcecf86334d9017e2422840e1b8f49ba04d
mojtaba_ni <mojtabanik@yahoo.com>
2025-03-22 17:30:15 +0330

    feat: Add menu item click handling and improve sidebar layout

--- app/shared/ui/panel/PanelSidebar.tsx
+++ app/shared/ui/panel/PanelSidebar.tsx
@@ -23,33 +23,63 @@ const LogoutButton = () => {
       <IconLogout />
       <span style={{ transform: "translateY(3px)" }}>Logout</span>
     </div>
-  )
-}
+  );
+};
 
-const PanelSidebar = () => {
+const PanelSidebar = ({
+  drawer,
+  close,
+}: {
+  drawer: boolean;
+  close: () => void;
+}) => {
   const [userMenu, setUserMenu] = useState<Array<UserMenu>>([]);
-  useEffect(()=>{
-    if(typeof window !== 'undefined'){
+  const [menuIndex, setMenuIndex] = useState(0);
+
+  const handleSideItem = (index: number) => {
+    setMenuIndex(index);
+    setTimeout(() => {
+      close();
+    }, 1000);
+  };
+  useEffect(() => {
+    if (typeof window !== "undefined") {
       setUserMenu(JSON.parse(localStorage.getItem("userMenu") || "[]"));
     }
   }, []);
   return (
-    <Card shadow="sm" padding="lg" className={style.wrapper}>
-      <Box>
-        <Image src={Title} alt="ChanciAi" loading="lazy" width={115} />
-      </Box>
-      <Divider color="#D5D5D7" style={{ margin: "1.6rem 0" }} />
+    <Card shadow="sm" className={style.wrapper}>
+      {!drawer && (
+        <>
+          <Box>
+            <Image src={Title} alt="ChanciAi" loading="lazy" width={115} />
+          </Box>
+          <Divider color="#D5D5D7" style={{ margin: "1.6rem 0" }} />
+        </>
+      )}
 
       <Box className={style.progressPart}>
         {userMenu?.map((menu, index) => {
           return (
-
-            <Link key={`menu-${index}`} href={`${menu.link}`} className={style.progressPartBox}>
+            <Link
+              key={`menu-${index}`}
+              href={`${menu.link}`}
+              onClick={() => handleSideItem(index)}
+              className={
+                menuIndex === index
+                  ? style.progressPartBoxActive
+                  : style.progressPartBox
+              }
+            >
               {/* <IconUser /> */}
               <span style={{ transform: "translateY(3px)" }}>{menu.title}</span>
             </Link>
-          )
+          );
         })}
+        <Link href={`/ChanciAI`} className={style.progressPartBox}>
+          {/* <IconUser /> */}
+          <span style={{ transform: "translateY(3px)" }}>Chanci AI</span>
+        </Link>
         <LogoutButton />
       </Box>
     </Card>
