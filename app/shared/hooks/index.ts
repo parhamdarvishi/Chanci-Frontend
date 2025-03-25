4c036d9fdc8115261806819e10e1db74f2bd64b2
Farid vm <flive73@gmail.com>
2025-03-25 03:39:48 +0330

    fix: test pre-commit

--- app/shared/hooks/index.ts
+++ app/shared/hooks/index.ts
@@ -4,18 +4,18 @@ const useIsMobile = (breakpoint: number = 768): boolean | undefined  =>{
     const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth < breakpoint);
 
   useEffect(() => {
-    const handleResizeTest = () => {
+    const handleResize = () => {
       setIsMobile(window.innerWidth < 768);
     };
 
     // Initial check
-    handleResizeTest();
+    handleResize();
 
     // Add event listener
-    window.addEventListener("resize", handleResizeTest);
+    window.addEventListener("resize", handleResize);
 
     // Cleanup on unmount
-    return () => window.removeEventListener("resize", handleResizeTest);
+    return () => window.removeEventListener("resize", handleResize);
   }, [breakpoint]);
 
   return isMobile;
