cee44a304b9005826fdd317f192bfb21f7a96619
mojtaba_ni <mojtabanik@yahoo.com>
2025-03-25 19:47:58 +0330

    feat: implement answer submission and navigation to results page in PsychologyTest component

--- app/widget/chanciAI/slice/PsychologyTest/psychologyTest.tsx
+++ app/widget/chanciAI/slice/PsychologyTest/psychologyTest.tsx
@@ -6,12 +6,11 @@ import UploadQuestion from "@/features/uploadQuestion/UploadQuestion";
 import { postRequest } from "@/shared/api";
 import { userAddresses } from "@/shared/constants/relative-url/user";
 import { useChanci } from "@/shared/stateManagement/UseChanci/useChanci";
-import { Card } from "@mantine/core";
 import React, { useEffect } from "react";
-import style from "../style.module.scss";
 import { modals } from "@mantine/modals";
 import StepModal from "../stepModal/stepModal";
 import MultiSelectBox from "@/features/multiSelect/MultiSelect";
+import { useRouter } from "next/navigation";
 
 interface Question {
   inputType?: number;
@@ -28,16 +27,20 @@ const PsychologyTest: React.FC<PsychologyTestProps> = ({ question }) => {
     updateQuestionIndex,
     answers,
     UpdateSidebarPostion,
-    sidebarPostion,
     data,
   } = useChanci();
-
+  const router = useRouter();
   const sendAnswers = async () => {
     const reqbody = {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
       answers: answers.map(({ step, ...rest }) => rest),
     };
-    await postRequest(userAddresses.userAnswers, reqbody, true);
+    const { data } = await postRequest(
+      userAddresses.userAnswers,
+      reqbody,
+      true
+    );
+    router.push(`/ChanciAI/result/${(data as { id: string }).id}`);
   };
   const openModal = () => {
     const desc =
@@ -85,57 +88,9 @@ const PsychologyTest: React.FC<PsychologyTestProps> = ({ question }) => {
       )}
       {question?.inputType === 4 && <UploadQuestion />}
       {question?.inputType === 5 && <MultiSelectBox question={question} />}
-      {sidebarPostion === 6 && data?.length === questionIndex && (
-        <div
-          style={{ display: "flex", flexDirection: "column", gap: "1.5rem" }}
-        >
-          <Card
-            shadow="sm"
-            padding="lg"
-            radius="md"
-            withBorder
-            className={style.cardDone}
-          >
-            <h3>Thank you for completing the test! 🎉</h3>
-            <p style={{ maxWidth: "700px", fontSize: "17px" }}>
-              Based on your responses, we&apos;ve prepared a detailed analysis
-              of your personality traits, career preferences, and strengths.
-            </p>
-            <p style={{ maxWidth: "700px" }}>
-              This report is designed to help you gain deeper insights into your
-              unique qualities and guide you toward making informed decisions
-              for your personal and professional growth.
-            </p>
-            <span style={{ fontSize: "17px", fontWeight: "600" }}>
-              Here&apos;s what you can expect in your results:
-            </span>
-          </Card>
-          <Card
-            shadow="sm"
-            padding="lg"
-            radius="md"
-            withBorder
-            className={style.cardDone}
-          >
-            <h3>Openness to Experience </h3>
-            <strong style={{ fontSize: "18px", fontWeight: "500" }}>
-              You scored high on openness.
-            </strong>
-            <li style={{ maxWidth: "700px", fontSize: "17px" }}>
-              Youre naturally curious and eager to explore new ideas and
-              experiences.
-            </li>
-            <li style={{ maxWidth: "700px", fontSize: "17px" }}>
-              You enjoy creative problem-solving and are enthusiastic about
-              learning new things.
-            </li>
-            <p style={{ maxWidth: "700px", fontSize: "17px" }}>
-              Example from your answers: You showed excitement about learning a
-              new instrument despite your busy schedule.
-            </p>
-          </Card>
-        </div>
-      )}
+      {/* {sidebarPostion === 6 && data?.length === questionIndex && (
+  
+      )} */}
     </div>
   );
 };
