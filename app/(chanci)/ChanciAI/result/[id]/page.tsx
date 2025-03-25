248fdd2213d325bd4832cf0ed4b4a29b093d18f8
mojtaba_ni <mojtabanik@yahoo.com>
2025-03-25 19:46:03 +0330

    feat: add results page for ChanciAI with personality analysis and insights

---
+++ app/(chanci)/ChanciAI/result/[id]/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { Card } from "@mantine/core";
+import React from "react";
+import style from "../../../style.module.scss";
+
+const Page = () => {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "1.5rem" }}>
+      <Card
+        shadow="sm"
+        padding="lg"
+        radius="md"
+        withBorder
+        className={style.cardDone}
+      >
+        <h3>Thank you for completing the test! 🎉</h3>
+        <p style={{ maxWidth: "700px", fontSize: "17px" }}>
+          Based on your responses, we&apos;ve prepared a detailed analysis of
+          your personality traits, career preferences, and strengths.
+        </p>
+        <p style={{ maxWidth: "700px" }}>
+          This report is designed to help you gain deeper insights into your
+          unique qualities and guide you toward making informed decisions for
+          your personal and professional growth.
+        </p>
+        <span style={{ fontSize: "17px", fontWeight: "600" }}>
+          Here&apos;s what you can expect in your results:
+        </span>
+      </Card>
+      <Card
+        shadow="sm"
+        padding="lg"
+        radius="md"
+        withBorder
+        className={style.cardDone}
+      >
+        <h3>Openness to Experience </h3>
+        <strong style={{ fontSize: "18px", fontWeight: "500" }}>
+          You scored high on openness.
+        </strong>
+        <li style={{ maxWidth: "700px", fontSize: "17px" }}>
+          Youre naturally curious and eager to explore new ideas and
+          experiences.
+        </li>
+        <li style={{ maxWidth: "700px", fontSize: "17px" }}>
+          You enjoy creative problem-solving and are enthusiastic about learning
+          new things.
+        </li>
+        <p style={{ maxWidth: "700px", fontSize: "17px" }}>
+          Example from your answers: You showed excitement about learning a new
+          instrument despite your busy schedule.
+        </p>
+      </Card>
+    </div>
+  );
+};
+
+export default Page;
