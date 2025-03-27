ed213610d3dd321435c013beabb97279bc1c8177
Farid vm <flive73@gmail.com>
2025-03-27 13:52:51 +0330

    blog slide updated in About Us page - third commit

--- app/widget/Candidates/model.ts
+++ app/widget/Candidates/model.ts
@@ -2,7 +2,7 @@ export interface SlideData {
   id: number;
   title: string;
   description: string;
-   
+  image?: any;//this line stays for static images on project
   bannerImagePath: string;
   link: string;
 }
