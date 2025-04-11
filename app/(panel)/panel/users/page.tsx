503702469508da4e622f7cbba3ac4ac49b514fae
mojtaba_ni <mojtabanik@yahoo.com>
2025-04-11 21:40:44 +0330

    fix:  imports and enhance functionality in users page component with role management modal

--- app/(panel)/panel/users/page.tsx
+++ app/(panel)/panel/users/page.tsx
@@ -1,25 +1,36 @@
 "use client";
 import { userAddresses } from "@/shared/constants/relative-url/user";
-import { TableColumns, TableOnRequest } from "@/shared/ui/Table";
+import { TableOnRequest } from "@/shared/ui/Table";
+import { ActionModal, TableColumns } from "@/shared/ui/Table/model";
+import RoleManagement from "@/widget/Actions/RoleManagement";
 import React from "react";
 
 type TUsers = {
   id?: string;
   index: number;
   userName?: string;
-  email: string
-}
+  email: string;
+};
 const columns: TableColumns<TUsers>[] = [
-  { head: "Index", key: "index"},
-  { head: 'Email', key: 'email' }
+  { head: "Index", key: "index" },
+  { head: "Email", key: "email" },
 ];
+
+const actionModal: ActionModal = {
+  title: "Role Management",
+  name: "Role",
+  component: <RoleManagement />,
+};
+
 const Page = () => {
-    return (
-      <TableOnRequest<TUsers>
-        rowsPerPage={10}
-        url={userAddresses.GetAll}
-        columns={columns}
-      />);
+  return (
+    <TableOnRequest<TUsers>
+      rowsPerPage={10}
+      url={userAddresses.GetAll}
+      columns={columns}
+      actionModal={actionModal}
+    />
+  );
 };
 
 export default Page;
