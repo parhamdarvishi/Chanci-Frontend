d798c5637933b6f44f87fe01e1482ef1b639b5c1
mojtaba_ni <mojtabanik@yahoo.com>
2025-04-11 21:40:56 +0330

    feat: add new user role endpoints for role management

--- app/shared/constants/relative-url/user.ts
+++ app/shared/constants/relative-url/user.ts
@@ -2,6 +2,9 @@ export const userAddresses = {
   GetAll: "/api/User/Get",
   profile: "/api/User/Profile",
   userAnswers: "/api/UserAnswers/Add",
+  usersRole: "/api/User/GetUserRoles",
+  getAllRole: "/api/Role/GetAll",
+  addNewRole: "/api/User/AddRole",
   ConvertAnswersToPromptCommand:
     "/api/UserAnswers/ConvertAnswersToPromptCommand",
 };
