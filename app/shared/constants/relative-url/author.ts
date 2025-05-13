0a32eb3fff0124e10676ef555b252dead64ea793
Parsa <parsa@greenhabitat.at>
2025-05-13 17:53:44 +0330

    fix blog description - author section - delete action

---
+++ app/shared/constants/relative-url/author.ts
@@ -0,0 +1,7 @@
+export const authorAddresses = {
+  GetAll: "/api/Author/GetAll",
+  GetById: "/api/Author/GetById",
+  Add: "/api/Author/Add",
+  Delete: "/api/Author/Delete",
+  Update: "/api/Author/Update",
+};
